79254fc51adb75ae4e0475ba8729549313a00bfe
Leo <ronald.mauritsson@gmail.com>
2020-02-18 19:08:03 +0300

    Created the book object using the constructor

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const myLibrary = [];
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = false;
+  this.status = function () {
+    if (this.isRead === true) {
+      return 'Read';
+    }
+    return 'Not Read'
+  }
+  this.info = function () {
+    return `${title} ,${author} ,${pages}pages${this.status()}`;
+  }
+}
+
+function addBookToLibrary(title, author, pages) {
+  const book = new Book(title, author, pages);
+  myLibrary.push(book);
+}
