b08f2f8ebd5b4fa5d630f3ce0a8d343e15a2a11b
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-02-18 10:49:34 -0600

    Readjust form and main.js

--- js/main.js
+++ js/main.js
@@ -9,14 +9,20 @@ function Book(title, author, pages) {
     if (this.isRead === true) {
       return 'Read';
     }
-    return 'Not Read'
-  }
+    return 'Not Read';
+  };
   this.info = function () {
     return `${title} ,${author} ,${pages}pages${this.status()}`;
-  }
+  };
 }
 
 function addBookToLibrary(title, author, pages) {
   const book = new Book(title, author, pages);
   myLibrary.push(book);
+}
+
+function render(form) {
+  const bookstr = form.inputbox.value;
+  document.write(`the book is ${bookstr}`);
+  document.write(`ets`);
 }
